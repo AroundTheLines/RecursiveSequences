e58202c780fbc08f55468eb4de4c3c277c795262
Qile Wang <wangqile123@gmail.com>
2015-06-17 08:22:53 -0400

    bleh

--- src/testThingy/Mclass.java
+++ src/testThingy/Mclass.java
@@ -4,13 +4,15 @@ public class Mclass {
 	
 	public static int M(int n){
 		if(n>100){
+			System.out.print((n-10)+"\t");
 			return n-10;
 		}else{
+			System.out.println((M(M(n+11)))+"\t");
 			return M(M(n+11));
 		}
 	}
 	
 	public static void main(String[] args){
-		M(121121);
+		M(99);
 	}
 }
